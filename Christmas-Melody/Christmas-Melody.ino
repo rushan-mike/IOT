860642a2f90df9a3ee5a6c03b97a8ed051811796
Michael <23063073+rushan-mike@users.noreply.github.com>
2021-12-05 20:19:34 +0530

    Knight-Rider-Sound.ino

---
+++ Christmas-Melody/Christmas-Melody.ino
@@ -0,0 +1,175 @@
+#include "pitches.h"
+#define melodyPin 3
+
+// Jingle Bells
+
+int Jingle_melody[] = {
+  NOTE_E5, NOTE_E5, NOTE_E5,
+  NOTE_E5, NOTE_E5, NOTE_E5,
+  NOTE_E5, NOTE_G5, NOTE_C5, NOTE_D5,
+  NOTE_E5,
+  NOTE_F5, NOTE_F5, NOTE_F5, NOTE_F5,
+  NOTE_F5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5,
+  NOTE_E5, NOTE_D5, NOTE_D5, NOTE_E5,
+  NOTE_D5, NOTE_G5
+};
+
+int Jingle_tempo[] = {
+  8, 8, 4,
+  8, 8, 4,
+  8, 8, 8, 8,
+  2,
+  8, 8, 8, 8,
+  8, 8, 8, 16, 16,
+  8, 8, 8, 8,
+  4, 4
+};
+
+// Santa Claus is coming to town
+
+int santa_melody[] = {
+  NOTE_G4,
+  NOTE_E4, NOTE_F4, NOTE_G4, NOTE_G4, NOTE_G4,
+  NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, NOTE_C5,
+  NOTE_E4, NOTE_F4, NOTE_G4, NOTE_G4, NOTE_G4,
+  NOTE_A4, NOTE_G4, NOTE_F4, NOTE_F4,
+  NOTE_E4, NOTE_G4, NOTE_C4, NOTE_E4,
+  NOTE_D4, NOTE_F4, NOTE_B3,
+  NOTE_C4
+};
+
+int santa_tempo[] = {
+  8,
+  8, 8, 4, 4, 4,
+  8, 8, 4, 4, 4,
+  8, 8, 4, 4, 4,
+  8, 8, 4, 2,
+  4, 4, 4, 4,
+  4, 2, 4,
+  1
+};
+
+// We wish you a merry Christmas
+
+int wish_melody[] = {
+  NOTE_B3, 
+  NOTE_F4, NOTE_F4, NOTE_G4, NOTE_F4, NOTE_E4,
+  NOTE_D4, NOTE_D4, NOTE_D4,
+  NOTE_G4, NOTE_G4, NOTE_A4, NOTE_G4, NOTE_F4,
+  NOTE_E4, NOTE_E4, NOTE_E4,
+  NOTE_A4, NOTE_A4, NOTE_B4, NOTE_A4, NOTE_G4,
+  NOTE_F4, NOTE_D4, NOTE_B3, NOTE_B3,
+  NOTE_D4, NOTE_G4, NOTE_E4,
+  NOTE_F4
+};
+
+int wish_tempo[] = {
+  4,
+  4, 8, 8, 8, 8,
+  4, 4, 4,
+  4, 8, 8, 8, 8,
+  4, 4, 4,
+  4, 8, 8, 8, 8,
+  4, 4, 8, 8,
+  4, 4, 4,
+  2
+};
+
+int number = 0;
+
+void setup(void) {
+  Serial.begin(9600);
+  Serial.print("Welcome");
+  Serial.println();
+  Serial.println();
+}
+
+void loop() {
+
+  if (Serial.available() > 0) {
+    number = Serial.readString().toInt();
+
+    if (number == 0) {
+        
+        Serial.print("----- Exit -----");
+        Serial.println();
+        Serial.end();
+    }
+
+    Serial.print("Playing song : " + String(number));
+    Serial.println();
+    Serial.println();
+    
+  }
+  
+  if (number == 1) sing(1); 
+  if (number == 2) sing(2);
+  if (number == 3) sing(3);
+}
+
+int song = 0;
+
+void sing(int s) {
+
+  song = s;
+  if (song == 1) {
+    Serial.println("'Jingle Bells'");
+    Serial.println();
+    int size = sizeof(Jingle_melody) / sizeof(int);
+    for (int thisNote = 0; thisNote < size; thisNote++) {
+      int noteDuration = 1200 / Jingle_tempo[thisNote];
+      tone(melodyPin, Jingle_melody[thisNote], noteDuration);
+    //   buzz(melodyPin, Jingle_melody[thisNote], noteDuration);
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+      noTone(melodyPin);
+    //   buzz(melodyPin, 0, noteDuration);
+    }
+    number = 0;
+  }
+
+  if (song == 2) {
+    Serial.println("'Santa Claus is coming to town'");
+    Serial.println();
+    int size = sizeof(santa_melody) / sizeof(int);
+    for (int thisNote = 0; thisNote < size; thisNote++) {
+      int noteDuration = 900 / santa_tempo[thisNote];
+      tone(melodyPin, santa_melody[thisNote], noteDuration);
+    //   buzz(melodyPin, santa_melody[thisNote], noteDuration);
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+      noTone(melodyPin);
+    //   buzz(melodyPin, 0, noteDuration);
+    }
+    number = 0;
+  }
+
+  if (song == 3) {
+    Serial.println("'We wish you a Merry Christmas'");
+    Serial.println();
+    int size = sizeof(wish_melody) / sizeof(int);
+    for (int thisNote = 0; thisNote < size; thisNote++) {
+      int noteDuration = 900 / wish_tempo[thisNote];
+      tone(melodyPin, wish_melody[thisNote], noteDuration);
+    //   buzz(melodyPin, wish_melody[thisNote], noteDuration);
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+      noTone(melodyPin);
+    //   buzz(melodyPin, 0, noteDuration);
+    }
+    number = 0;
+  } 
+}
+
+// void buzz(int targetPin, long frequency, long length) {
+//   digitalWrite(targetPin, HIGH);
+//   long delayValue = 1000000 / frequency / 2;
+//   long numCycles = frequency * length / 1000;
+//   for (long i = 0; i < numCycles; i++) {
+//     digitalWrite(targetPin, HIGH);
+//     delayMicroseconds(delayValue);
+//     digitalWrite(targetPin, LOW);
+//     delayMicroseconds(delayValue);
+//   }
+//   digitalWrite(targetPin, LOW);
+// }
--- Knight-Rider-Sound/Knight-Rider-Sound.ino
+++ Knight-Rider-Sound/Knight-Rider-Sound.ino
@@ -1,7 +1,133 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
+#include "pitches.h"
+#define melodyPin 3
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+int knight_melody[] = {
+    // 1
+    NOTE_A4, NOTE_AS4, NOTE_A4, 
+    NOTE_A4, NOTE_AS4, NOTE_A4, NOTE_A4, 
+    NOTE_AS4, NOTE_A4, NOTE_A4, NOTE_A4, 
+    NOTE_GS4, NOTE_A4, NOTE_A4, NOTE_A4, 
+    NOTE_A4, NOTE_AS4, NOTE_A4, 
+    NOTE_A4, NOTE_AS4, NOTE_A4, NOTE_A4, 
+    NOTE_AS4, NOTE_A4, NOTE_A4, NOTE_A4, 
+    NOTE_GS4, NOTE_A4, NOTE_A4, NOTE_A4, 
+    NOTE_G4, NOTE_GS4, NOTE_G4, 
+    NOTE_G4, NOTE_GS4, NOTE_G4, NOTE_G4, 
+    NOTE_GS4, NOTE_G4, NOTE_G4, NOTE_G4, 
+    NOTE_FS4, NOTE_G4, NOTE_G4, NOTE_G4, 
+    NOTE_G4, NOTE_GS4, NOTE_G4, 
+    NOTE_G4, NOTE_GS4, NOTE_G4, NOTE_G4, 
+    NOTE_GS4, NOTE_G4, NOTE_G4, NOTE_G4, 
+    NOTE_FS4, NOTE_G4, NOTE_G4, NOTE_G4, 
+    // 2
+    NOTE_A4, NOTE_AS4, NOTE_A4, 
+    NOTE_A4, NOTE_AS4, NOTE_A4, NOTE_A4, 
+    NOTE_AS4, NOTE_A4, NOTE_A4, NOTE_A4, 
+    NOTE_GS4, NOTE_A4, NOTE_A4, NOTE_A4, 
+    NOTE_A4, NOTE_AS4, NOTE_A4, 
+    NOTE_A4, NOTE_AS4, NOTE_A4, NOTE_A4, 
+    NOTE_AS4, NOTE_A4, NOTE_A4, NOTE_A4, 
+    NOTE_GS4, NOTE_A4, NOTE_A4, NOTE_A4, 
+    NOTE_G4, NOTE_GS4, NOTE_G4, 
+    NOTE_G4, NOTE_GS4, NOTE_G4, NOTE_G4, 
+    NOTE_GS4, NOTE_G4, NOTE_G4, NOTE_G4, 
+    NOTE_FS4, NOTE_G4, NOTE_G4, NOTE_G4, 
+    NOTE_G4, NOTE_GS4, NOTE_G4, 
+    NOTE_G4, NOTE_GS4, NOTE_G4, NOTE_G4, 
+    NOTE_GS4, NOTE_G4, NOTE_G4, NOTE_G4, 
+    NOTE_FS4, NOTE_G4, NOTE_G4, NOTE_G4, 
+    // 3
+    NOTE_A4, NOTE_AS4, NOTE_A4,
+    NOTE_A4, NOTE_AS4, NOTE_A4, NOTE_A4, 
+    NOTE_AS4, NOTE_A4, NOTE_A4, NOTE_A4, 
+    NOTE_GS4, NOTE_A4, NOTE_A4, NOTE_A4, 
+    NOTE_A4, NOTE_AS4, NOTE_A4, 
+    NOTE_A4, NOTE_AS4, NOTE_A4, NOTE_A4, 
+    NOTE_AS4, NOTE_A4, NOTE_A4, NOTE_A4, 
+    NOTE_GS4, NOTE_A4, NOTE_A4, NOTE_A4, 
+    NOTE_G4, NOTE_GS4, NOTE_G4, 
+    NOTE_G4, NOTE_GS4, NOTE_G4, NOTE_G4, 
+    NOTE_GS4, NOTE_G4, NOTE_G4, NOTE_G4, 
+    NOTE_FS4, NOTE_G4, NOTE_G4, NOTE_G4, 
+    NOTE_G4, NOTE_GS4, NOTE_G4, 
+    NOTE_G4, NOTE_GS4, NOTE_G4, NOTE_G4, 
+    NOTE_GS4, NOTE_G4, NOTE_G4, NOTE_G4, 
+    NOTE_FS4, NOTE_G4, NOTE_G4, NOTE_G4, 
+    // solo 
+    NOTE_A4, NOTE_AS4, NOTE_A4, NOTE_E5, 
+    NOTE_A5, NOTE_AS5, NOTE_A5, NOTE_E5, 
+    NOTE_A4, NOTE_AS4, NOTE_A4, NOTE_E5, NOTE_A5, NOTE_G5, 
+    NOTE_A4, NOTE_AS4, NOTE_A4, NOTE_E5, 
+    NOTE_A5, NOTE_AS5, NOTE_A5, NOTE_E5, 
+    NOTE_A4, NOTE_AS4, NOTE_A4, NOTE_E5, NOTE_A5, NOTE_AS5, NOTE_G5, NOTE_A5
+};
+
+
+
+int knight_tempo[] = {
+    // 1
+    250, 125, 125, 
+    125, 125, 125, 125, 
+    125, 125, 125, 125, 
+    125, 125, 125, 125, 
+    250, 125, 125, 
+    125, 125, 125, 125, 
+    125, 125, 125, 125, 
+    125, 125, 125, 125, 
+    250, 125, 125, 
+    125, 125, 125, 125, 
+    125, 125, 125, 125, 
+    125, 125, 125, 125, 
+    250, 125, 125, 
+    125, 125, 125, 125, 
+    125, 125, 125, 125, 
+    125, 125, 125, 125, 
+    // 2
+    250, 125, 125, 
+    125, 125, 125, 125, 
+    125, 125, 125, 125, 
+    125, 125, 125, 125, 
+    250, 125, 125, 
+    125, 125, 125, 125, 
+    125, 125, 125, 125, 
+    125, 125, 125, 125, 
+    250, 125, 125, 
+    125, 125, 125, 125, 
+    125, 125, 125, 125, 
+    125, 125, 125, 125, 
+    250, 125, 125, 
+    125, 125, 125, 125, 
+    125, 125, 125, 125, 
+    125, 125, 125, 125, 
+    // 3
+    250, 125, 125, 
+    125, 125, 125, 125, 
+    125, 125, 125, 125, 
+    125, 125, 125, 125, 
+    250, 125, 125, 
+    125, 125, 125, 125, 
+    125, 125, 125, 125, 
+    125, 125, 125, 125, 
+    250, 125, 125, 
+    125, 125, 125, 125, 
+    125, 125, 125, 125, 
+    125, 125, 125, 125, 
+    250, 125, 125, 
+    125, 125, 125, 125, 
+    125, 125, 125, 125, 
+    125, 125, 125, 125, 
+    // solo 
+    250, 125, 125, 1500, 
+    250, 125, 125, 1500, 
+    250, 125, 125, 250, 250, 2000, 
+    250, 125, 125, 1500, 
+    250, 125, 125, 1500, 
+    250, 125, 125, 250, 250, 2500, 250, 500
+};
 
 int brightness = 0;
 int time = 5;
@@ -49,6 +175,8 @@ void loop(){
     delay(time*100 - time);
 
     displayDOWN();
+
+    soundUP();
   }
 
 }
@@ -110,4 +238,22 @@ void displayDOWN(){
   lcd.noBacklight();
   lcd.clear();
 
+}
+
+
+void soundUP() {
+
+  int size = sizeof(knight_melody) / sizeof(int);
+
+  for (int thisNote = 0; thisNote < size; thisNote++) {
+
+    int noteDuration = knight_tempo[thisNote];
+    tone(melodyPin, knight_melody[thisNote], noteDuration);
+
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+
+    noTone(melodyPin);
+
+  }
 }
